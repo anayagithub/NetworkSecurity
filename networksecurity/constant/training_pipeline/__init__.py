918df47d705cd3c615764a401ccafaa9e5f25d48
Anaya Jain <anayajain1009@gmail.com>
2025-11-18 02:19:06 +0530

    Transformation done

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -40,3 +40,19 @@ DATA_VALIDATION_INVALID_DIR: str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+
+
+
+'''
+DATA TRANSFORMATION RELATED CONSTANT START WITH DATA_TRANSFORMATION VAR NAME
+'''
+DATA_TRANSFORMATION_DIR_NAME= "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR="transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR=" transformed_object"
+
+# knn inputer to replace nan values
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict={
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform",
+}
--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -2,11 +2,9 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionArtifact:
-    trained_file_path: str
-    test_file_path: str
+    trained_file_path:str
+    test_file_path:str
 
-    
-    
 @dataclass
 class DataValidationArtifact:
     validation_status: bool
@@ -14,7 +12,22 @@ class DataValidationArtifact:
     valid_test_file_path: str
     invalid_train_file_path: str
     invalid_test_file_path: str
-    drift_report_file_path: str    
+    drift_report_file_path: str
 
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str
+
+# @dataclass
+# class ClassificationMetricArtifact:
+#     f1_score: float
+#     precision_score: float
+#     recall_score: float
     
-    
+# @dataclass
+# class ModelTrainerArtifact:
+#     trained_model_file_path: str
+#     train_metric_artifact: ClassificationMetricArtifact
+#     test_metric_artifact: ClassificationMetricArtifact
